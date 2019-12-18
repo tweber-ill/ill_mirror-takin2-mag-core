fb58c5fcca8f355871bacc0a931f7745fe8f0a06
Tobias WEBER <tweber@ill.fr>
2019-12-18 07:58:01 +0100

    initialisation order

--- tools/moldyn/moldyn.cpp
+++ tools/moldyn/moldyn.cpp
@@ -1182,14 +1182,12 @@ void MolDynDlg::keyPressEvent(QKeyEvent *evt)
 
 int main(int argc, char** argv)
 {
-	set_gl_format(1, _GL_MAJ_VER, _GL_MIN_VER, 8);
+	set_gl_format(1, _GL_MAJ_VER, _GL_MIN_VER, 0);
 	tl2::set_locales();
 
-	QApplication::addLibraryPath(QApplication::applicationDirPath());
-	QApplication::addLibraryPath(QApplication::applicationDirPath() + QDir::separator() + "qtplugins");
 	QApplication::addLibraryPath(QString(".") + QDir::separator() + "qtplugins");
-
 	auto app = std::make_unique<QApplication>(argc, argv);
+	QApplication::addLibraryPath(QApplication::applicationDirPath() + QDir::separator() + "qtplugins");
 
 	auto dlg = std::make_unique<MolDynDlg>(nullptr);
 	dlg->show();
